06b1cf9855b66c0597f9d81ce47189c6fd1a6447
s-hens <108551327+s-hens@users.noreply.github.com>
2023-05-22 17:05:57 +0100

    Add JS-based validation to 'postcode' field

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//--------------------//
+// Show/hide password //
+//--------------------//
 document.querySelectorAll(".visibility").forEach(button => {
     button.addEventListener("click", togglePasswordVisibility);
 })
@@ -9,5 +12,28 @@ function togglePasswordVisibility() {
     } else if (this.previousElementSibling.getAttribute("type") == "text") {
         this.previousElementSibling.setAttribute("type", "password");
         this.innerText = "visibility_off";
-    } else return;
+    }
+}
+
+//-----------------//
+// Validate inputs //
+//-----------------//
+
+// Postcode
+const postcode = document.getElementById("postcode");
+const postcodeError = document.querySelector(".postcode-error");
+
+postcode.addEventListener("change", validatePostcode);
+postcode.addEventListener("input", validatePostcode);
+postcode.addEventListener("focusout", validatePostcode);
+
+function validatePostcode(e) {
+    if (e.type == "change" && postcode.validity.patternMismatch == true ||
+        e.type == "focusout" && postcode.validity.valueMissing == true) {
+        postcodeError.innerText = "Please enter a valid UK postcode. Example: SW1A 1AA";
+        document.getElementById("postcode").classList.add("invalid");
+    } else if (e.type == "input" && !postcode.validity.patternMismatch) {
+        postcodeError.innerText = "";
+        document.getElementById("postcode").classList.remove("invalid");
+    }
 }
