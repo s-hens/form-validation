3f5529fad06bf7618a81fa782e2cdbc02a93a6b7
s-hens <108551327+s-hens@users.noreply.github.com>
2023-05-22 17:20:08 +0100

    Add JS validation to 'country' field

--- script.js
+++ script.js
@@ -19,6 +19,24 @@ function togglePasswordVisibility() {
 // Validate inputs //
 //-----------------//
 
+// Country
+const country = document.getElementById("country");
+const countryError = document.querySelector(".country-error");
+
+country.addEventListener("change", validateCountry);
+country.addEventListener("input", validateCountry);
+country.addEventListener("focusout", validateCountry);
+
+function validateCountry() {
+    if (country.validity.valueMissing) {
+        countryError.innerText = "Please select your country.";
+        country.classList.add("invalid");
+    } else if (!country.validity.valueMissing) {
+        countryError.innerText = "";
+        country.classList.remove("invalid");
+    }
+}
+
 // Postcode
 const postcode = document.getElementById("postcode");
 const postcodeError = document.querySelector(".postcode-error");
@@ -28,14 +46,11 @@ postcode.addEventListener("input", validatePostcode);
 postcode.addEventListener("focusout", validatePostcode);
 
 function validatePostcode(e) {
-    if (e.type == "change" && postcode.validity.patternMismatch == true ||
-        e.type == "focusout" && postcode.validity.valueMissing == true) {
-
+    if (e.type == "change" && postcode.validity.patternMismatch ||
+        e.type == "focusout" && postcode.validity.valueMissing) {
         postcodeError.innerText = "Please enter a valid UK postcode. Example: SW1A 1AA";
         postcode.classList.add("invalid");
-
     } else if (e.type == "input" && !postcode.validity.patternMismatch) {
-
         postcodeError.innerText = "";
         postcode.classList.remove("invalid");
     }
@@ -50,14 +65,11 @@ email.addEventListener("input", validateEmail);
 email.addEventListener("focusout", validateEmail);
 
 function validateEmail(e) {
-    if (e.type == "change" && email.validity.typeMismatch == true ||
-        e.type == "focusout" && email.validity.valueMissing == true) {
-
+    if (e.type == "change" && email.validity.typeMismatch ||
+        e.type == "focusout" && email.validity.valueMissing) {
         emailError.innerText = "Please enter an email address.";
         email.classList.add("invalid");
-
     } else if (e.type == "input" && !email.validity.typeMismatch) {
-        
         emailError.innerText = "";
         email.classList.remove("invalid");
     }
