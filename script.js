7b38a2a231f6af13adda304dc7ac1041706ddf8a
s-hens <108551327+s-hens@users.noreply.github.com>
2023-05-22 17:10:54 +0100

    Add JS validation to 'email' field

--- script.js
+++ script.js
@@ -30,10 +30,35 @@ postcode.addEventListener("focusout", validatePostcode);
 function validatePostcode(e) {
     if (e.type == "change" && postcode.validity.patternMismatch == true ||
         e.type == "focusout" && postcode.validity.valueMissing == true) {
+
         postcodeError.innerText = "Please enter a valid UK postcode. Example: SW1A 1AA";
-        document.getElementById("postcode").classList.add("invalid");
+        postcode.classList.add("invalid");
+
     } else if (e.type == "input" && !postcode.validity.patternMismatch) {
+
         postcodeError.innerText = "";
-        document.getElementById("postcode").classList.remove("invalid");
+        postcode.classList.remove("invalid");
+    }
+}
+
+// Email
+const email = document.getElementById("email");
+const emailError = document.querySelector(".email-error");
+
+email.addEventListener("change", validateEmail);
+email.addEventListener("input", validateEmail);
+email.addEventListener("focusout", validateEmail);
+
+function validateEmail(e) {
+    if (e.type == "change" && email.validity.typeMismatch == true ||
+        e.type == "focusout" && email.validity.valueMissing == true) {
+
+        emailError.innerText = "Please enter an email address.";
+        email.classList.add("invalid");
+
+    } else if (e.type == "input" && !email.validity.typeMismatch) {
+        
+        emailError.innerText = "";
+        email.classList.remove("invalid");
     }
 }
